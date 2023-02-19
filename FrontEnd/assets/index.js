96b6d4a3ebf61544bd6cf198213901886fadf3f7
dkoshevko <dkoshevko@gmail.com>
2023-02-20 00:31:42 +0100

    mise en place d'une fenetre modale

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -18,6 +18,10 @@ const getCategories = () => {
         .catch((error) => { console.log( error ) });
 };
 
+
+
+
+// Vérification si l'utilisateur est autentifié en tant qu'administrateur
 let userLogInToken = window.sessionStorage.getItem("loggedUser");
 
 if (userLogInToken !== null ) {
@@ -26,12 +30,17 @@ if (userLogInToken !== null ) {
     const logInLogOut = document.getElementById("login-logout");
     logInLogOut.innerText = "logout";
 
+    // Afficher le bouton "Modifier"
+    document.getElementById('change-works-btn').style.display = null;
+
     logInLogOut.addEventListener("click", function (event) {
         event.preventDefault();
         window.sessionStorage.removeItem("loggedUser");
         window.location.reload();
     });
 };
+
+
 
 
 // Déclaration de fonction qui permet de générer les travaux 
@@ -120,4 +129,72 @@ filtersSection.addEventListener("click", function(btn) {
         document.querySelector(".filters-container button.active").classList.remove("active");
     }
     btn.target.classList.add("active")
+});
+
+
+
+
+// Création de la fenêtre modale
+let modal = null;
+const focusableSelector = 'button, a, input, textarea';
+let focusables = [];
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute('href'));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    modal.style.display = null;
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+};
+
+const closeModal = function (e) {
+    if (modal === null) return;
+    e.preventDefault();
+    window.setTimeout(function () {
+        modal.style.display = "none";
+        modal = null;
+    }, 500);
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+};
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+};
+
+const focusInModal = function (e) {
+    e.preventDefault();
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'));
+    if (e.shiftKey === true) {
+        index--;
+    } else {
+        index++;
+    };
+    if (index >= focusables.length) {
+        index = 0;
+    };
+    if (index < 0) {
+        index = focusables.length - 1;
+    };
+    focusables[index].focus();
+};
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    };
+    if (e.key === 'Tab' && modal !== null) {
+        focusInModal(e);
+    };
 });
